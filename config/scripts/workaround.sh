2904a50d3c8c6c1047e9e4b26743cd75774fa205
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2024-02-17 20:22:13 +0700

    feat: add docker and vscode

---
+++ config/scripts/workaround.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oueux pipefail
+
+# Your code goes here.
+# echo 'This is an example shell script'
+# echo 'Scripts here will run during build if specified in recipe.yml'
+# alternatives cannot create symlinks on its own during a container build
+ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
