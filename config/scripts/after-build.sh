5c8d2e3fefa7752bb889aff9703b7cbca373fe45
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2024-03-03 04:36:59 +0700

    fix: remove rpm-ostree cleanup

--- config/scripts/after-build.sh
+++ config/scripts/after-build.sh
@@ -4,7 +4,6 @@
 # You should have this in every custom script, to ensure that your completed
 # builds actually ran successfully without any errors!
 set -oueux pipefail
-rpm-ostree cleanup -m && \
 ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld && \
 rm -f /etc/yum.repos.d/vscode.repo && \
 rm -f /etc/yum.repos.d/docker-ce.repo && \
