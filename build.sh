a9a22dd0314ad5a7af7703269f9908929f560ab5
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2024-08-04 12:27:51 +0700

    fix: build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # add repo 
-curl -Lo /etc/yum.repos.d/hardware:razer-"${FEDORA_MAJOR_VERSION}".repo https://download.opensuse.org/repositories/hardware:/razer/Fedora_"${FEDORA_MAJOR_VERSION}"/hardware:razer.repo
+curl -Lo /etc/yum.repos.d/hardware:razer-"${RELEASE}".repo https://download.opensuse.org/repositories/hardware:/razer/Fedora_"${RELEASE}"/hardware:razer.repo
 
 # this installs a package from fedora repos
 rpm-ostree install openrazer-daemon polychromatic python3-openrazer jetbrains-mono-fonts-all fira-code-fonts langpacks-id
